f0cb8a99aa1483ebcf11117d639ecd402a1eee4c
ChrisApost <apostolov1475@students.d211.org>
2018-01-24 13:28:09 -0600

    I committed

--- app/src/main/java/com/example/rc211/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rc211/myapplication/MainActivity.java
@@ -1,28 +1,52 @@
 package com.example.rc211.myapplication;
 
-import android.app.Activity;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import com.example.rc211.myapplication.Game.GameEventScheduler;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends Activity {
+    private ProgressBar mProgressBar;
+    private TextView mLoadingText;
 
+    private int mStatus = 100;
 
-    private static GameEventScheduler gamehandler;
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gamehandler = new GameEventScheduler();
+        mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
+        mLoadingText = (TextView) findViewById(R.id.LoadingCompleteTextView);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mStatus < 101){
+                    mStatus--;
+                    android.os.SystemClock.sleep(50);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgressBar.setProgress(mStatus);
+                        }
+                    });
+                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mLoadingText.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        }).start();
 
     }
 
 
-
-
-
-
-    
-    
 }
