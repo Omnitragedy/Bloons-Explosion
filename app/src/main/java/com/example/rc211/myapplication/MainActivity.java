6bffc182d3f9729889d56bc7273b9fc6cd09c071
Alec <panayotov2325@students.d211.org>
2018-01-29 13:36:57 -0600

    fth

--- app/src/main/java/com/example/rc211/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rc211/myapplication/MainActivity.java
@@ -9,8 +9,13 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.view.View.OnTouchListener;
+import android.widget.RelativeLayout;
+import android.app.Activity;
 
 import com.example.rc211.myapplication.Game.GameEventScheduler;
 
@@ -28,6 +33,11 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     private GameView gameView;
 
+    private ImageView img;
+    private ViewGroup rootLayout;
+    private int _xDelta;
+    private int _yDelta;
+
     public MainActivity() {
         mHandler = new Handler();
     }
@@ -42,6 +52,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         gamehandler = new GameEventScheduler();
 
+        rootLayout = (ViewGroup) findViewById(R.id.view_root);
+        img = (ImageView) rootLayout.findViewById(R.id.imageView);
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+        img.setLayoutParams(layoutParams);
+        img.setOnTouchListener(new ChoiceTouchListener());
 
         new Thread(new Runnable() {
             @Override
@@ -137,4 +153,37 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
 
     }
+
+
+    private final class ChoiceTouchListener implements OnTouchListener {
+        public boolean onTouch(View view, MotionEvent event){
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    _xDelta = X - lParams.leftMargin;
+                    _yDelta = Y - lParams.topMargin;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    layoutParams.leftMargin = X - _xDelta;
+                    layoutParams.topMargin = Y - _yDelta;
+                    layoutParams.rightMargin = -250;
+                    layoutParams.bottomMargin = -250;
+                    view.setLayoutParams(layoutParams);
+                    break;
+            }
+            rootLayout.invalidate();
+            return true;
+        }
+    }
+
+
 }
